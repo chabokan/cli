723e893625ef623b1d723a79f4dd9be8a660ab0a
Mohammad <mohammad_abdi76@yahoo.com>
2022-05-23 14:04:59 +0430

    update

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ import * as os from "os";
 
 
 export const GLOBAL_CONF_PATH = path.join(os.homedir(), '.chabok.json');
-export const BASE_API_URL = "https://hub.chabokan.net/api/v1/";
+export const BASE_API_URL = "https://hub.chabokan.net/fa/api/v1/";
